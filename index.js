b3f3ec67448dde040ad05281acf7c5a4995d6cbe
guillaume <guillaury.gv40@gmail.com>
2022-09-23 10:39:59 +0200

    anim titre v1

--- index.js
+++ index.js
@@ -11,3 +11,43 @@ animationOver.forEach(element => {
 });
 
 
+const target = document.getElementById("target");
+let array = ["bienvenue", "sur", "mon", "site"];
+let worldIndex = 0;
+let letterIndex = 0;
+
+const creatLettter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.textContent = array[worldIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 2000);
+}; 
+
+const loop = () => {
+  setTimeout(() => {
+    // si on ne reviens pas 0
+    if (worldIndex >= array.length) {
+      worldIndex = 0;
+      letterIndex = 0;
+      loop();
+    //   alors création d'une nouvelle lettre
+    } else if (letterIndex < array[worldIndex].length) {
+      creatLettter();
+      letterIndex++;
+      loop();
+    }
+    // alors changement de mot
+    else {
+      worldIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2000);
+    }
+  }, 80);
+};
+loop();
